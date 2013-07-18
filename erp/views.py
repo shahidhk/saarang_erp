71ab21f3af7ff3c3414e37a08972b4603189c815
Shahidh K Muhammed <muhammedshahid.k@gmail.com>
2013-07-18 13:43:54 +0530

    Views error

--- erp/views.py
+++ erp/views.py
@@ -14,61 +14,61 @@ import datetime
 
 @login_required(login_url='/login/')
 def home(request):
-	'''
-		Renders the home page, display the name and a welcome message, have to change to preferred view
-		based on user type
-	'''
-	name=request.user.first_name + " " + request.user.last_name
-	return render_to_response('home.html', {'user': name}, context_instance=RequestContext(request))
-	
+    '''
+        Renders the home page, display the name and a welcome message, have to change to preferred view
+        based on user type
+    '''
+    name=request.user.first_name + " " + request.user.last_name
+    return render_to_response('home.html', {'user': name}, context_instance=RequestContext(request))
+    
 def login_user(request):
-	'''
-		Login handler
-		Have to change all the HttpResponse to Alerts
-	'''
-	if request.user.is_authenticated():
-		return HttpResponse('You are already logged in')
-	else:
-		if request.method == 'POST':
-			username = request.POST['username']
-			password = request.POST['password']
-			user = authenticate(username=username, password=password)
-			if user is not None:
-				if user.is_active:
-					login(request, user) # log in the user
-					return redirect('erp.views.home')
-				else:
-					return HttpResponse('<center>Account not active, contact Admin</center>')
-			else:
-				print 'invalid'
-				return HttpResponse('<center>Invalid Login</center>')
-		else:
-			next="home"
-			return render_to_response('login.html', {'next': next}, context_instance=RequestContext(request))
+    '''
+        Login handler
+        Have to change all the HttpResponse to Alerts
+    '''
+    if request.user.is_authenticated():
+        return HttpResponse('You are already logged in')
+    else:
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user) # log in the user
+                    return redirect('erp.views.home')
+                else:
+                    return HttpResponse('<center>Account not active, contact Admin</center>')
+            else:
+                print 'invalid'
+                return HttpResponse('<center>Invalid Login</center>')
+        else:
+            next="home"
+            return render_to_response('login.html', {'next': next}, context_instance=RequestContext(request))
 
 def logout_user(request):
-	'''
-		Logs out a user
-	'''
+    '''
+        Logs out a user
+    '''
     logout(request)
     return HttpResponse('<center>logged out</center>')
 
 def page(request):
-	'''
-		Gen test view
-	'''
-	users_in_coords = Group.objects.get(name="Coords").user_set.all()
-	users_in_cores = Group.objects.get(name="Cores").user_set.all()
-	users_in_convenors = Group.objects.get(name="Convenors").user_set.all()
+    '''
+        Gen test view
+    '''
+    users_in_coords = Group.objects.get(name="Coords").user_set.all()
+    users_in_cores = Group.objects.get(name="Cores").user_set.all()
+    users_in_convenors = Group.objects.get(name="Convenors").user_set.all()
 
-	if request.user in users_in_coords:
-		state='Coord'
-	elif request.user in users_in_cores:
-		state='Core'
-	elif request.user in users_in_convenors:
-		state='Convenor'
-	else:
-		state='Unknown'
-	html='Hello %s , Welcome to saarang erp, you are a %s ' % (request.user.username, state)
-	return HttpResponse(html)
+    if request.user in users_in_coords:
+        state='Coord'
+    elif request.user in users_in_cores:
+        state='Core'
+    elif request.user in users_in_convenors:
+        state='Convenor'
+    else:
+        state='Unknown'
+    html='Hello %s , Welcome to saarang erp, you are a %s ' % (request.user.username, state)
+    return HttpResponse(html)
 
