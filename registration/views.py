70baeb336eab8c1f3080f039a33ee8c6107377f3
Shahidh K Muhammed <muhammedshahid.k@gmail.com>
2013-11-02 14:07:44 +0530

    Added mainsite registrations, minor fix to ui

---
+++ registration/views.py
@@ -0,0 +1,38 @@
+# Create your views here.
+from django.shortcuts import render
+from forms import SaarangUserForm
+from models import SaarangUser
+
+def add_user(request):
+    if request.method == 'POST':
+        userform =SaarangUserForm(request.POST)
+        if userform.is_valid():
+            SaarangUserForm.save()
+        else:
+            userform = SaarangUserForm(request.POST)
+    else:
+        userform = SaarangUserForm()
+    to_return={
+            'form':userform,
+            'action':  "",
+            'title': "Add a new User"
+        }
+    return render(request, 'user_registration/new.html', to_return)
+
+def list_users(request):
+    users = SaarangUser.objects.all()
+    to_return={
+            'users':users,
+            'action':  "",
+            'title': "Registered users"
+        }
+    return render(request, 'user_registration/list.html', to_return)
+
+def show_user(request, user_id):
+    user = SaarangUser.objects.get(pk=user_id)
+    to_return={
+            'user':user,
+            'action':  "",
+            'title': "Registerd user details"
+        }
+    return render(request, 'user_registration/user.html', to_return)
