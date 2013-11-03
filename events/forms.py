32f34c05698524b7906c7944b13f2903fe9baf5e
Shahidh K Muhammed <muhammedshahid.k@gmail.com>
2013-11-04 03:07:12 +0530

    Fixed Select2

--- events/forms.py
+++ events/forms.py
@@ -1,6 +1,7 @@
 # From django
 from django import forms
 from django.contrib.auth.models import Group
+from django.forms.widgets import SelectMultiple
 
 #From models
 from models import Event
@@ -8,6 +9,7 @@ from models import Event
 class CreateEventForm(forms.ModelForm):
     COORDS = [[coord.id, coord.first_name] for coord in Group.objects.get(name="coord").user_set.all()]
     coords = forms.MultipleChoiceField(choices=COORDS, required=False)
+    coords.widget.attrs.update({'id': 'multiselect', 'placeholder': 'Select coordinators', 'style':'width:300px'})
     class Meta:
         model = Event
         fields = ['name', 'long_name', 'sub_dept','google_group', 'email']
@@ -15,10 +17,11 @@ class CreateEventForm(forms.ModelForm):
 class EventForm(forms.ModelForm):
     COORDS = [[coord.id, coord.first_name] for coord in Group.objects.get(name="coord").user_set.all()]
     coords = forms.MultipleChoiceField(choices=COORDS, required=False)
+    coords.widget.attrs.update({'id': 'multiselect', 'placeholder': 'Select coordinators', 'style':'width:300px'})
     class Meta:
         model = Event
         fields = ['long_name','google_group','oneliner', 'email']
-
+    
 class EventRegistrationForm(forms.ModelForm):
 	class Meta:
 		model = Event
