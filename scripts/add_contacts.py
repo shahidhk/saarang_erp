e6cb516684e7810999f739bde7b295c7a2e2b2e9
Saarango <noreply@saarang.org>
2013-11-12 02:29:19 +0530

    Modified scripts

--- scripts/add_contacts.py
+++ scripts/add_contacts.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 def update_contacts():
     events = Event.objects.all()
     for event in events:
-        text = '<p>For futher queries contact <a href="mailto:'+event.email+ '">' + event.email + '</a></p><br/><p><b>Coordinators:</b></p><p><ol>'
+        text = '<p>For futher queries, contact <a href="mailto:'+event.email+ '">' + event.email + '</a></p><br/><p><b>Coordinators:</b></p><p><ol>'
         for user in event.user_events.all():
             coord = User.objects.get(pk=user.user_id)
-            text += '<li>' + coord.first_name +', Email: <a href="mailto:'+ coord.email+'">'+coord.email+'</a>, Phone: '+str(coord.userprofile.mobile)+'</li>'
+            text += '<li>' + coord.first_name +'<br/>'+str(coord.userprofile.mobile)+'</li>'
         text+='</ol></p>'
         event.contacts = text
         event.save()
