11c2bbee1b22f3547233b8d26e66439476d7b15d
Shahidh K Muhammed <muhammedshahid.k@gmail.com>
2013-12-10 03:10:01 +0530

    Script modified to limit connections

--- scripts/hospi_release_mail.py
+++ scripts/hospi_release_mail.py
@@ -2,7 +2,7 @@ from registration.models import SaarangUser
 from django.core import mail
 i=1
 
-def get_email():
+def get_email(start, step):
     i=1
     text_file = open('scripts/email.txt', 'r')
     html_file = open('scripts/email.html', 'r')
@@ -12,19 +12,19 @@ def get_email():
     html_file.close()
     subject = 'Accommodation Portal open - Saarang 2014'
     fr = 'webadmin@saarang.org'
-    users = SaarangUser.objects.all()
+    users = SaarangUser.objects.filter(pk__range=(start,start+step))
     email_list = []
     for user in users:
         msg = mail.EmailMultiAlternatives(subject, text, fr, [user.email])
         msg.attach_alternative(html, "text/html")
         email_list.append(msg)
-        print i, user.email, ' parsed'
+        print i, user.pk, user.email, ' parsed'
         i+=1
     return email_list
 
-def send_emails():
+def send_emails(start, step):
     print 'Initialising ...'
     connection = mail.get_connection()
-    messages = get_email()
+    messages = get_email(start, step)
     connection.send_messages(messages)
     print 'Emails sent'
