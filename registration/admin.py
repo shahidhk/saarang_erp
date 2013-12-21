9a3d08c6cfff2c40be885035389d513658d94eab
Shahidh K Muhammed <muhammedshahid.k@gmail.com>
2013-12-21 22:59:33 +0530

    Admin fix

--- registration/admin.py
+++ registration/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from models import SaarangUser
 
-admin.site.register(SaarangUser)
+class SaarangUserAdmin(admin.ModelAdmin):
+    list_display=('saarang_id', 'email', 'name', 'mobile', 'gender')
+    search_fields = ['email', 'saarang_id', 'name']
+
+admin.site.register(SaarangUser, SaarangUserAdmin)
--- registration/forms.py
+++ registration/forms.py
@@ -5,4 +5,4 @@ from models import SaarangUser
 class SaarangUserForm(forms.ModelForm):
     class Meta:
         model = SaarangUser
-        exclude = ['saarang_id', 'friend_list', 'fb_token', 'activate_status']
+        exclude = ['saarang_id', 'friend_list', 'fb_token', 'college_id', 'activate_status', 'fb_id', 'accomod_is_confirmed']
