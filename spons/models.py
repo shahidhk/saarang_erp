996f11644d31ddc6229ee21fa4dce18f9c9d5236
Shahidh K Muhammed <muhammedshahid.k@gmail.com>
2013-12-22 16:38:47 +0530

    Added url to spons

--- spons/models.py
+++ spons/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 class SponsImageUpload(models.Model):
     title = models.CharField(max_length=1000,verbose_name='Title',help_text='Example:Title Sponsor, Website Sponsor')
+    sponsor_link = models.URLField(max_length=500, help_text='Example: http://company.com')
     logo = models.ImageField(upload_to='spons')
     timestamp = models.DateTimeField(auto_now_add=True)
     uploaded_by = models.ForeignKey(User, related_name='image_uploaded_by')
